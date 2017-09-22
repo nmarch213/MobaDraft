579ee82b279ba86d7a49b47e65924f3d659fcfb1
Nicholas March <n.march213@gmail.com>
2017-09-22 14:53:15 -0500

    Initial Server Side setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
