d6fde4e56a117cb022c7c588218a27fceae652f5
Nicholas March <n.march213@gmail.com>
2017-09-24 18:15:54 -0500

    Moved from Materilize to Bootstrap 4

--- client/src/components/hots/HotsNewDraft.js
+++ client/src/components/hots/HotsNewDraft.js
@@ -15,7 +15,7 @@ const HotsNewDraft = props => {
               className="validate"
               label="Team 1 Name"
             />
-            <label for="teamOneName">Team 1 Name</label>
+            <label htmlFor="teamOneName">Team 1 Name</label>
           </div>
           <div className="input-field col s6">
             <Field
@@ -24,7 +24,33 @@ const HotsNewDraft = props => {
               type="text"
               className="validate"
             />
-            <label for="teamTwoName">Team 2 Name</label>
+            <label htmlFor="teamTwoName">Team 2 Name</label>
+          </div>
+          <div className="input-field col s12">
+            <Field name="map" component="select">
+              <option />
+              <option value="BOE">Battlefield of Eternity</option>
+              <option value="Hanamura">Hanamura</option>
+              <option value="Towers of Doom">Towers of Doom</option>
+              <option value="Garden of Terror">Garden of Terror</option>
+              <option value="Dragon Shire">Dragon Shire</option>
+              <option value="Blackheart's Bay">Blackheart's Bay</option>
+              <option value="Cursed Hollow">Cursed Hollow</option>
+              <option value="Warhead Junction">Warhead Junction</option>
+              <option value="Braxis Holdout">Braxis Holdout</option>
+              <option value="Tomb">Tomb of the Spider Queen</option>
+              <option value="Sky Temple">Sky Temple</option>
+              <option value="Infernal Shrines">Infernal Shrines</option>
+              <option value="Haunted Mines">Haunted Mines</option>
+            </Field>
+          </div>
+          <div>
+            <Field name="favoriteColor" component="select">
+              <option />
+              <option value="ff0000">Red</option>
+              <option value="00ff00">Green</option>
+              <option value="0000ff">Blue</option>
+            </Field>
           </div>
         </div>
       </form>
