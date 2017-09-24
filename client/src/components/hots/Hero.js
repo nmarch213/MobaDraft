b1cf3a240e375bf9cd6b8eb76b80be172f586212
Nicholas March <n.march213@gmail.com>
2017-09-24 13:23:51 -0500

    Heroes Component displaying

--- client/src/components/hots/Hero.js
+++ client/src/components/hots/Hero.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 
 class Hero extends Component {
-  renderHeroes() {
-    return _.map(this.props.variables, hero => (
-      <a href={hero.url}>
-        <div className="card col s6 m1 l1" key={hero.id}>
-          <div className="card-image">
-            <img src={hero.portrait.medium} />
-            {/* <div className="card-content">{hero.name}</div> */}
-          </div>
-        </div>
-      </a>
-    ));
-  }
-
   render() {
-    console.log('hots: ', this.props.variables);
-    const { variables } = this.props;
-
-    if (!variables) {
-      return <div>Loading...</div>;
-    }
     return (
-      <div className="row">
-        <div className="col">{this.renderHeroes()}</div>
+      <div className="container">
+        <a href={this.props.hero.url}>
+          <div className="card col s3 m2 l2">
+            <div className="card-image">
+              <img src={this.props.hero.portrait.medium} />
+            </div>
+          </div>
+        </a>
       </div>
     );
   }
--- client/src/components/hots/HotsDraftHeroPool.js
+++ client/src/components/hots/HotsDraftHeroPool.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Hero from './Hero';
 
 class HotsDraftHeroPool extends Component {
+  renderHeroPool() {
+    return _.map(this.props.hotsHeroes, hero => <Hero hero={hero} />);
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="container align-center">
         <h2>Heroes</h2>
+        <div className="row">{this.renderHeroPool()}</div>
       </div>
     );
   }
